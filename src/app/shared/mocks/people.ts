96bb24ef570a64d92d8c0fadd8c2adfdd44f238e
tonygoussot <tonygoussot@users.noreply.github.com>
2017-02-24 21:31:05 +0100

    chore(data): update 24022017

--- src/app/shared/mocks/people.ts
+++ src/app/shared/mocks/people.ts
@@ -1,19 +1,18 @@
 export const PEOPLE: Array<Object> = [
-  {surname: 'François', name: 'Fillon',         imgurl: 'W6Nr3S', selector: 'fillon',     filteris: 0.2275, elabe: 0.2100, program:'49dxtk', color: '#1A237E', party: "Les Républicains"},
-  {surname: 'Marine',   name: 'Le Pen',         imgurl: 'i86EuI', selector: 'lepen',      filteris: 0.2142, elabe: 0.2800, program:'ZLVtcS', color: '#283593', party: "Front National"},
-  {surname: 'Emmanuel', name: 'Macron',         imgurl: 'TziZ4B', selector: 'macron',     filteris: 0.1980, elabe: 0.1850, program:'rspDlS', color: '#90CAF9', party: "En Marche!"},
-  {surname: 'Jean-luc', name: 'Melenchon',      imgurl: 'ji1GLi', selector: 'melenchon',  filteris: 0.1700, elabe: 0.1300, program:'14Iw5v', color: '#C2185B', party: "France Insoumise"},
-  {surname: 'Benoit',   name: 'Hamon',          imgurl: 'IXeO7i', selector: 'hamon',      filteris: 0.1352, elabe: 0.1300, program:'ndPHgh', color: '#EC407A', party: "Parti Socialiste"},
-  {surname: 'Nicolas',  name: 'Dupont-Aignan',  imgurl: 'UdlArO', selector: 'dupont',     filteris: 0.0298, elabe: 0.0000, program:'IWHLrY', color: '#6A1B9A', party: "Debout la France"},
-  {surname: 'Yannick',  name: 'Jadot',          imgurl: 'urq5yJ', selector: 'jadot',      filteris: 0.0207, elabe: 0.0000, program:'d0d1iw', color: '#64DD17', party: "Europe Écologie Les Verts"},
-  {surname: 'Philippe', name: 'Poutou',         imgurl: 'C08gY6', selector: 'poutou',     filteris: 0.0015, elabe: 0.0000, program:'kljuxy', color: '#F44336', party: "Nouveau Parti anticapitaliste"},
-  {surname: 'Nathalie', name: 'Arthaud',        imgurl: 'CCKY77', selector: 'arthaud',    filteris: 0.0013, elabe: 0.0000, program:'cILzs5', color: '#D50000', party: "Lutte ouvrière"},
-  {surname: 'François', name: 'Asselineau',     imgurl: 'FMns4f', selector: 'asselineau', filteris: 0.0011, elabe: 0.0000, program:'pwMfc1', color: '#00897B', party: "Union populaire républicaine"},
-  {surname: 'Jacques',  name: 'Cheminade',      imgurl: 'OIMMIR', selector: 'cheminade',  filteris: 0.0007, elabe: 0.0000, program:'532sr5', color: '#FFA000', party: "Solidarité et progrès"},
+  {surname: 'François', name: 'Fillon',         imgurl: 'W6Nr3S', selector: 'fillon',     filteris: 0.2253, elabe: 0.2100, program:'49dxtk', color: '#1A237E', party: "Les Républicains"},
+  {surname: 'Marine',   name: 'Le Pen',         imgurl: 'i86EuI', selector: 'lepen',      filteris: 0.2174, elabe: 0.2800, program:'ZLVtcS', color: '#283593', party: "Front National"},
+  {surname: 'Emmanuel', name: 'Macron',         imgurl: 'TziZ4B', selector: 'macron',     filteris: 0.2016, elabe: 0.1850, program:'rspDlS', color: '#90CAF9', party: "En Marche!"},
+  {surname: 'Jean-luc', name: 'Melenchon',      imgurl: 'ji1GLi', selector: 'melenchon',  filteris: 0.1522, elabe: 0.1300, program:'14Iw5v', color: '#C2185B', party: "France Insoumise"},
+  {surname: 'Benoit',   name: 'Hamon',          imgurl: 'IXeO7i', selector: 'hamon',      filteris: 0.1518, elabe: 0.1300, program:'ndPHgh', color: '#EC407A', party: "Parti Socialiste"},
+  {surname: 'Nicolas',  name: 'Dupont-Aignan',  imgurl: 'UdlArO', selector: 'dupont',     filteris: 0.0356, elabe: 0.0000, program:'IWHLrY', color: '#6A1B9A', party: "Debout la France"},
+  {surname: 'Philippe', name: 'Poutou',         imgurl: 'C08gY6', selector: 'poutou',     filteris: 0.0059, elabe: 0.0000, program:'kljuxy', color: '#F44336', party: "Nouveau Parti anticapitaliste"},
+  {surname: 'Nathalie', name: 'Arthaud',        imgurl: 'CCKY77', selector: 'arthaud',    filteris: 0.0055, elabe: 0.0000, program:'cILzs5', color: '#D50000', party: "Lutte ouvrière"},
+  {surname: 'François', name: 'Asselineau',     imgurl: 'FMns4f', selector: 'asselineau', filteris: 0.0032, elabe: 0.0000, program:'pwMfc1', color: '#00897B', party: "Union populaire républicaine"},
+  {surname: 'Jacques',  name: 'Cheminade',      imgurl: 'OIMMIR', selector: 'cheminade',  filteris: 0.0016, elabe: 0.0000, program:'532sr5', color: '#FFA000', party: "Solidarité et progrès"},
   {surname: 'Olivier',  name: 'Régis',          imgurl: 'Pdrcoe', selector: 'regis',      filteris: 0.0000, elabe: 0.0000, program:'RPpwCV', color: '#8D6E63', party: "Aimer la France"}
 ];
 
-export const METADATA: Object = {update: '2017-02-22T20:25:28+01:00'};
+export const METADATA: Object = {update: '2017-02-24T21:30:24+01:00'};
 
 
 // TODO   Sieges à l'assemblée
